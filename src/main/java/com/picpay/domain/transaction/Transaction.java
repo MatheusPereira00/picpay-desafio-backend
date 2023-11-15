6993864d9aca9261de53530d535c746d18f2b4d3
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-15 11:10:42 -0300

    feat: add transaction entity

---
+++ src/main/java/com/picpay/domain/transaction/Transaction.java
@@ -0,0 +1,35 @@
+package com.picpay.domain.transaction;
+
+
+import com.picpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name= "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name= "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
